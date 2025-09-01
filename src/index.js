6201889ceefec758c08b14b540f9ea88f33c69d8
rakoc001 <chris_rakowski4@hotmail.com>
2025-09-01 15:49:04 +0100

    updating src

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode, Fragment } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './containers/App';
+import 'tachyons';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <>
+      <App />
+    </>
+  </StrictMode>
+);
