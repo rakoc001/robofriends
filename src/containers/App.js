8e31ec2936e0c8dbfea72887d21895d26de70037
rakoc001 <chris_rakowski4@hotmail.com>
2025-09-02 13:36:46 +0100

    removing commented lines

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,20 +7,7 @@ import './App.css';
 function App() {
     const [ robots, setRobots ] = useState([]);
     const [ searchfield, setSearchfield ] = useState('');
-// class App extends React.Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             robots: [],
-//             searchfield: ''
-//         }
-//     }
-
-//     componentDidMount() {
-//         fetch('https://jsonplaceholder.typicode.com/users')
-//         .then(response => response.json())
-//         .then(users => this.setState({robots: users}))
-//     }
+    
     const callAPI = () => {
         return fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
@@ -35,8 +22,7 @@ function App() {
     const onSearchChange = (event) => {
         setSearchfield(event.target.value) 
     };
-
-    // const {robots, searchfield } = this.state;  
+    
     const filteredRobots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
